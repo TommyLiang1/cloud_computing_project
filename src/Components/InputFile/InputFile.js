d5b0c5ffc629133fcd023d5be2eddd88ea521cb3
TommyLiang1 <tommyliang131@gmail.com>
2022-11-06 20:03:50 -0500

    Implementing Amplify First Commit

--- src/Components/InputFile/InputFile.js
+++ src/Components/InputFile/InputFile.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
-import Amplify, { Storage } from "aws-amplify";
+import { Amplify, Storage } from "aws-amplify";
 import awsconfig from '../../aws-exports';
 import './InputFile.css';
 
-// Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
-// document.getElementById('file-upload').addEventListener('submit', e => {
-//   e.preventDefault();
-//   const file = document.getElementById('file-upload').files[0];
+let tmp = document.getElementById('upload-form');
+if(tmp) {
+  tmp.addEventListener('submit', e => {
+    e.preventDefault();
+    const file = document.getElementById('file-upload').files[0];
 
-//   Storage.put(file.name, file)
-//     .then(item => {
-//       console.log(item);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     })
-// })
+    Storage.put(file.name, file)
+      .then(item => {
+        console.log(item);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
+} else {
+  console.log(tmp);
+}
 
 class InputFile extends Component {
   render() {
     return (
-      <form id="input-container">
-        <input class="button" id="file-upload" type="file" name="filename" accept=".mp3"/>
-        <input class="button-36" type="submit" value="Upload"/>
+      <form id="upload-form">
+        <input className="button" id="file-upload" type="file" name="filename" accept=".mp3"/>
+        <input className="button-36" type="submit" value="Upload"/>
       </form>
     );  
   }
