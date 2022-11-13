700e16d42780e7bee05e7f2c3777f09c5a8c3f31
TommyLiang1 <tommyliang131@gmail.com>
2022-11-13 00:35:33 -0500

    Added s3 bucket to store and load mp3 files

--- src/Components/InputFile/InputFile.js
+++ src/Components/InputFile/InputFile.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
+import Amplify, { Storage } from "aws-amplify";
+import awsconfig from '../../aws-exports';
 import './InputFile.css';
+
+Amplify.configure(awsconfig);
 
 const InputFile = () => {
   useEffect(() => {
     document.getElementById('upload-form').addEventListener('submit', e => {
       e.preventDefault();
       const file = document.getElementById('file-upload').files[0];
-      console.log(file);
+      //console.log(file);
       
       const fileurl = URL.createObjectURL(file);
       
@@ -19,22 +23,46 @@ const InputFile = () => {
       source.setAttribute("type", "audio/mpeg")
       document.querySelector('.tracks').appendChild(audio);
 
-      // Storage.put(file.name, file)
-      //   .then(item => {
-      //     console.log(item);
-      //   })
-      //   .catch(err => {
-      //     console.log(err);
-      //   })
+      Storage.put(file.name, file)
+        .then(item => {
+          console.log(item);
+        })
+        .catch(err => {
+          console.log(err);
+        })
     })
   }, [])
+
+  Storage.list('')
+    .then(result => {
+      result.forEach(item => {
+        if(item.key !== '') {
+          Storage.get(item.key).then(result => {
+          const audio = document.createElement('audio');
+          const source = document.createElement('source');
+          audio.appendChild(source);
+          audio.setAttribute("controls","");
+          source.setAttribute("src", result);
+          source.setAttribute("type", "audio/mpeg");
+
+          document.querySelector('.tracks').appendChild(audio);
+          })
+        }
+      })
+    })
+    .catch(err => console.log(err));
+
   return (
     <div>
       <form id="upload-form">
         <input className="button" id="file-upload" type="file" name="filename" accept=".mp3"/>
         <input className="button-36" type="submit" value="Upload"/>
       </form>
-      <div className="tracks"></div>
+      <div className="playlist">
+        <h1>Mp3 Playlist</h1>
+        <div className="tracks">
+      </div>
+      </div>
     </div>
   );
 }
