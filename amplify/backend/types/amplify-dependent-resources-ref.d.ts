e289c6dc30b30ff5a6e66f854fc6026f996a893f
TommyLiang1 <tommyliang131@gmail.com>
2022-11-15 10:38:20 -0500

    Adding amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cloudcomputingauth": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "mp3Files": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
